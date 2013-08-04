b522a4a76ee9e8f98a7dc37511e6059b511efac7
Brad Dielman <bdielman@gmail.com>
2013-08-03 23:34:49 -0400

    Add files to test

--- test/test-creation.js
+++ test/test-creation.js
@@ -23,11 +23,26 @@ describe('baseline generator', function () {
     var expected = [
       // add files you expect to exist here.
       '.jshintrc',
-      '.editorconfig'
+      '.editorconfig',
+      '.gitignore',
+      'Gruntfile.js',
+      'Gemfile',
+      'package.json',
+      'bower.json',
+      'dev/index.html',
+      'dev/sass/styles.scss',
+      'dev/sass/base/_settings.scss',
+      'dev/sass/base/_helpers.scss',
+      'dev/sass/base/_base.scss',
+      'dev/sass/base/_content.scss',
+      'dev/sass/layout/_layout.scss',
+      'dev/sass/modules/_buttons.scss',
+      'dev/sass/modules/_media.scss',
+      'dev/sass/states/_display.scss'
     ];
 
     helpers.mockPrompt(this.app, {
-      'someOption': 'Y'
+      'projectName': 'Test Project'
     });
     this.app.options['skip-install'] = true;
     this.app.run({}, function () {
